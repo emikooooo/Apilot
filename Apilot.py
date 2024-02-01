98479a37e82a23f05fb37bfa34f5de33da08dfbc
emikooooo <128385199+emikooooo@users.noreply.github.com>
2024-02-01 10:18:36 +0800

    123123

--- Apilot.py
+++ Apilot.py
@@ -329,12 +329,12 @@ class Apilot(Plugin):
     def get_hot_trends(self, hot_trends_type):
         # 查找映射字典以获取API参数
         hot_trends_type_en = hot_trend_types.get(hot_trends_type, None)
-        payload = f"token=Pv9NigNNblo6nxCs&type={hot_trends_type_en}"
+        payload = "token=Pv9NigNNblo6nxCs&id=mproPpoq6O&type=weibo"
         headers = {'Content-Type': "application/x-www-form-urlencoded"}
         if hot_trends_type_en is not None:
             url = "https://v2.alapi.cn/api/tophub"
             try:
-                data = self.make_request("GET", url, data=payload, headers=headers)
+                data = self.make_request("POST", url, data=payload, headers=headers)
                 if isinstance(data, dict) and data['success'] == True:
                     output = []
                     topics = data['data']
