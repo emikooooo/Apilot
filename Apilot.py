e58924c55f31bd982a76d9f91460847fa44084cc
emikooooo <128385199+emikooooo@users.noreply.github.com>
2024-04-11 15:21:04 +0800

    1234

--- Apilot.py
+++ Apilot.py
@@ -528,7 +528,7 @@ class Apilot(Plugin):
                         for item in sorted_result:
                             time = item['uphis'][:5]
                             if time >= target_time:
-                                results.append(f"|{bank_name}|{currency_name}|{currency_name}|{item['se_sell']}|")
+                                results.append(f"|{bank_name}|{target_time}|{currency_name}|{item['se_sell']}|")
                                 break
                     else:
                         print("汇率获取失败，请稍后再试")
